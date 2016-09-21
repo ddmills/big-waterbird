7a7ebde1551ea79b91297eb5add2614b2237ba45
ddmills <dmills@iastate.edu>
2016-09-21 17:45:34 -0500

    add player health and health bar

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,19 +2,17 @@
 using System.Collections;
 
 public class Bullet : MonoBehaviour {
-
-    // Use this for initialization
-    void Start () {
-        
-    }
-
-    // Update is called once per frame
-    void Update () {
-        
-    }
-
-    void OnCollisionEnter()
+    void OnCollisionEnter(Collision collision)
     {
+        GameObject hit = collision.gameObject;
+
+        Health health = hit.GetComponent<Health>();
+
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthbar;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Player has died...");
+        }
+
+        healthbar.sizeDelta = new Vector2(currentHealth * 2, healthbar.sizeDelta.y);
+    }
+}
