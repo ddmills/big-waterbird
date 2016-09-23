cfc5b7716ddab111c904befc5edc5ac7183f998e
ddmills <mills.dalton@gmail.com>
2016-09-22 21:18:27 -0500

    make player faster

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : NetworkBehaviour {
         }
 
         float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 6.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
