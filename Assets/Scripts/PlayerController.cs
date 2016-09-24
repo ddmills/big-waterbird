863bf9b31432e48f8e17cc2b420c1fd43e48e3f4
ddmills <mills.dalton@gmail.com>
2016-09-24 12:23:19 -0500

    add mouselook

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,11 +15,10 @@ public class PlayerController : NetworkBehaviour {
             return;
         }
 
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 6.0f;
         float z = Input.GetAxis("Vertical") * Time.deltaTime * 6.0f;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        transform.Translate(x, 0, z);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -42,5 +41,10 @@ public class PlayerController : NetworkBehaviour {
 
         Camera.main.transform.SetParent(cameraHandle);
         Camera.main.transform.localPosition = Vector3.zero;
+
+        MouseLook mouseLook = Camera.main.GetComponent<MouseLook>();
+
+        mouseLook.TargetHorizontal = transform;
+        mouseLook.TargetVertical = cameraHandle.transform;
     }
 }
---
+++ Assets/scripts/MouseLook.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class MouseLook : MonoBehaviour
+{
+    public bool lockCursor = false;
+
+    private Transform targetHorizontal;
+    private Transform targetVertical;
+    private Vector2 targetHorizontalDirection;
+    private Vector2 targetVerticalDirection;
+
+    public Transform TargetHorizontal
+    {
+        get
+        {
+            return targetHorizontal;
+        }
+
+        set
+        {
+            targetHorizontal = value;
+            targetHorizontalDirection = targetHorizontal.localRotation.eulerAngles;
+        }
+    }
+
+    public Transform TargetVertical
+    {
+        get
+        {
+            return targetVertical;
+        }
+
+        set
+        {
+            targetVertical = value;
+            targetVerticalDirection = targetVertical.localRotation.eulerAngles;
+        }
+    }
+
+    public float sensitivityHorizontal = 15F;
+    public float sensitivityVertical = 15F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationHorizontal = 0F;
+    float rotationVertical = 0F;
+
+    void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void Update()
+    {
+        rotationHorizontal += Input.GetAxis("Mouse X") * sensitivityHorizontal;
+        rotationVertical += Input.GetAxis("Mouse Y") * sensitivityVertical;
+        rotationVertical = ClampAngle(rotationVertical, minimumY, maximumY);
+
+        if (targetHorizontal)
+        {
+            targetHorizontal.localRotation = Quaternion.AngleAxis(rotationHorizontal, targetHorizontal.up);
+            targetHorizontal.localRotation *= Quaternion.Euler(targetHorizontalDirection);
+        }
+
+        if (targetVertical)
+        {
+            targetVertical.localRotation = Quaternion.AngleAxis(rotationVertical, Vector3.right * -1);
+            targetVertical.localRotation *= Quaternion.Euler(targetVerticalDirection);
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F) {
+            angle += 360F;
+        }
+
+        if (angle > 360F) {
+            angle -= 360F;
+        }
+
+        return Mathf.Clamp(angle, min, max);
+    }
+}
