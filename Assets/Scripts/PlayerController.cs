6c6dcb05e88565e6b8ba3a66dad2adcaa95c9026
ddmills <mills.dalton@gmail.com>
2016-09-25 00:11:20 -0500

    character controls are getting closer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,37 +2,60 @@
 using System.Collections;
 using UnityEngine.Networking;
 
+[RequireComponent (typeof (CharacterController))]
 public class PlayerController : NetworkBehaviour {
 
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public Transform cameraHandle;
-    public float moveSpeed = 5;
-    public float jumpforce = 20;
 
-    // Update is called once per frame
-    void Update () {
+    public float moveSpeed = 8.0f;
+    public float jumpForce = 7.0f;
+    public float airModifier = 0.4f;
+    public float terminalVerticalVelocity = -10f;
+
+    Vector3 previousDeltas = Vector3.zero;
+
+    float verticalVelocity;
+
+    CharacterController characterController;
+
+    void Update() {
         if (!isLocalPlayer)
         {
             return;
         }
 
-        float x = Input.GetAxis("Horizontal") * moveSpeed;
-        float z = Input.GetAxis("Vertical") * moveSpeed;
+        float inputX = Input.GetAxis("Horizontal") * moveSpeed;
+        float inputZ = Input.GetAxis("Vertical") * moveSpeed;
 
-        Vector3 speed = transform.rotation * new Vector3(x, 0, z);
+        float modifier = moveSpeed;
 
-        //transform.Translate(x, 0, z);
 
-        CharacterController cc = GetComponent<CharacterController>();
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
 
-        cc.SimpleMove(speed);
+        if (characterController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = jumpForce;
+            }
+            else
+            {
+                verticalVelocity = terminalVerticalVelocity/2;
+            }
+        }
 
+        Vector3 deltas = new Vector3(inputX, verticalVelocity, inputZ);
+        deltas = transform.rotation * deltas;
+        previousDeltas = deltas;
+
+        characterController.Move(deltas * Time.deltaTime);
+        
         if (Input.GetMouseButtonDown(0))
         {
             CmdFire();
         }
-
     }
 
     [Command]
@@ -46,6 +69,8 @@ public class PlayerController : NetworkBehaviour {
 
     public override void OnStartLocalPlayer()
     {
+        characterController = GetComponent<CharacterController>();
+
         GetComponent<MeshRenderer>().material.color = Color.cyan;
 
         Camera.main.transform.SetParent(cameraHandle);
