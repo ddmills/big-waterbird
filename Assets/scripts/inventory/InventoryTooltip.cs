bf2d41264d7c0cbfbd038198ba60ffc655845e7b
ddmills <mills.dalton@gmail.com>
2016-10-02 16:40:14 -0500

    wired up behaviors to buttons

--- Assets/scripts/inventory/InventoryTooltip.cs
+++ Assets/scripts/inventory/InventoryTooltip.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using System.Collections;
+using System.Collections.Generic;
 
 public class InventoryTooltip : MonoBehaviour, IPointerExitHandler
 {
     private Loot item;
     private Text title;
     private CanvasGroup canvasGroup;
+    public GameObject behaviorButtonPrefab;
+    private List<GameObject> behaviorButtons = new List<GameObject>();
 
     void Start()
     {
@@ -21,9 +23,27 @@ public class InventoryTooltip : MonoBehaviour, IPointerExitHandler
         this.item = item;
         Debug.Log(item.title);
         title.text = item.title;
+        AddBehaviors();
         canvasGroup.alpha = 1;
         canvasGroup.interactable = true;
         canvasGroup.blocksRaycasts = true;
+    }
+
+    private void AddBehaviors()
+    {
+        foreach (GameObject button in behaviorButtons)
+        {
+            Destroy(button);
+        }
+
+        foreach (LootBehavior behavior in item.behaviors)
+        {
+            GameObject behaviorButton = Instantiate(behaviorButtonPrefab);
+            behaviorButton.GetComponentInChildren<Text>().text = behavior.verb;
+            behaviorButton.GetComponent<Button>().onClick.AddListener(behavior.Perform);
+            behaviorButton.transform.SetParent(transform);
+            behaviorButtons.Add(behaviorButton);
+        }
     }
 
     public void Deactivate()
